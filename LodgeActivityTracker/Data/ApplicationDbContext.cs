59255314ccfbd024f9e14b088c673b0327c5da8e
LERATO LESHABANE <leshabanellh91@gmail.com>
2026-01-29 15:00:25 +0200

    updated status and ran migration for adding status.

--- LodgeActivityTracker/Data/ApplicationDbContext.cs
+++ LodgeActivityTracker/Data/ApplicationDbContext.cs
@@ -1,11 +1,10 @@
 ﻿using LodgeActivityTracker.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LodgeActivityTracker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
