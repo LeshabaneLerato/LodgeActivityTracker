fb8e3749d68ae70d43202e94b195212358e1ffc1
LERATO LESHABANE <leshabanellh91@gmail.com>
2026-01-29 12:33:42 +0200

    fixed edit and create button

--- LodgeActivityTracker/Controllers/ActivitiesController.cs
+++ LodgeActivityTracker/Controllers/ActivitiesController.cs
@@ -1,123 +1,102 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using LodgeActivityTracker.Data;
+﻿using LodgeActivityTracker.Data;
 using LodgeActivityTracker.Models;
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
-namespace LodgeActivityTracker.Controllers
+public class ActivitiesController : Controller
 {
-    public class ActivitiesController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public ActivitiesController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-        private const int PageSize = 5; // number of activities per page
+        _context = context;
+    }
 
-        public ActivitiesController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    // GET: Activities
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Activities.ToListAsync());
+    }
 
-        // GET: Activities
-        public IActionResult Index(int page = 1)
-        {
-            var activities = _context.Activities
-                .OrderBy(a => a.Date);
+    // GET: Activities/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-            int totalItems = activities.Count();
-            int totalPages = (int)Math.Ceiling(totalItems / (double)PageSize);
-
-            var pagedActivities = activities
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize)
-                .ToList();
-
-            ViewBag.CurrentPage = page;
-            ViewBag.TotalPages = totalPages;
-
-            return View(pagedActivities);
-        }
-
-        // GET: Activities/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Activities/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Activity activity)
-        {
-            if (ModelState.IsValid)
-            {
-                activity.Status = "Pending"; // default status
-                _context.Activities.Add(activity);
-                _context.SaveChanges();
-
-                // Redirect to homepage after creating activity
-                return RedirectToAction("Index", "Home");
-            }
-
+    // POST: Activities/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Activity activity)
+    {
+        if (!ModelState.IsValid)
             return View(activity);
-        }
 
-        // GET: Activities/Edit/5
-        public IActionResult Edit(int id)
-        {
-            var activity = _context.Activities.Find(id);
-            if (activity == null)
-            {
-                return NotFound();
-            }
+        _context.Activities.Add(activity);
+        await _context.SaveChangesAsync();
+
+        TempData["SuccessMessage"] = "Activity created successfully!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Activities/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var activity = await _context.Activities.FindAsync(id);
+        if (activity == null)
+            return NotFound();
+
+        return View(activity);
+    }
+
+    // POST: Activities/Edit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Activity activity)
+    {
+        if (!ModelState.IsValid)
             return View(activity);
-        }
 
-        // POST: Activities/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Activity activity)
+        _context.Activities.Update(activity);
+        await _context.SaveChangesAsync();
+
+        TempData["SuccessMessage"] = "Activity updated successfully!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Activities/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var activity = await _context.Activities
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (activity == null)
+            return NotFound();
+
+        return View(activity);
+    }
+
+    // POST: Activities/DeleteConfirmed/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var activity = await _context.Activities.FindAsync(id);
+        if (activity != null)
         {
-            if (id != activity.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                _context.Update(activity);
-                _context.SaveChanges();
-
-                // Redirect to homepage after edit (user-friendly)
-                return RedirectToAction("Index", "Home");
-            }
-
-            return View(activity);
+            _context.Activities.Remove(activity);
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = "Activity deleted successfully!";
         }
 
-        // GET: Activities/Delete/5
-        public IActionResult Delete(int id)
-        {
-            var activity = _context.Activities.Find(id);
-            if (activity == null)
-            {
-                return NotFound();
-            }
-            return View(activity);
-        }
-
-        // POST: Activities/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var activity = _context.Activities.Find(id);
-            if (activity != null)
-            {
-                _context.Activities.Remove(activity);
-                _context.SaveChanges();
-            }
-
-            // Redirect to homepage after delete
-            return RedirectToAction("Index", "Home");
-        }
+        return RedirectToAction(nameof(Index));
     }
 }
--- LodgeActivityTracker/Helpers/QRCodeHelper.cs
+++ LodgeActivityTracker/Helpers/QRCodeHelper.cs
@@ -3,19 +3,22 @@ using System;
 using System.Drawing;
 using System.IO;
 
-public static class QRCodeHelper
+namespace LodgeActivityTracker.Helpers
 {
-    public static string GenerateQRCodeBase64(string text, int pixelsPerModule = 20)
+    public static class QRCodeHelper
     {
-        using (var qrGenerator = new QRCodeGenerator())
-        using (var qrData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q))
-        using (var qrCode = new QRCode(qrData))
-        using (var bitmap = qrCode.GetGraphic(pixelsPerModule))
-        using (var ms = new MemoryStream())
+        public static string GenerateQRCodeBase64(string text, int pixelsPerModule = 20)
         {
-            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-            var base64 = Convert.ToBase64String(ms.ToArray());
-            return $"data:image/png;base64,{base64}";
+            using (var qrGenerator = new QRCodeGenerator())
+            using (var qrData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q))
+            using (var qrCode = new QRCode(qrData))
+            using (var bitmap = qrCode.GetGraphic(pixelsPerModule))
+            using (var ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                var base64 = Convert.ToBase64String(ms.ToArray());
+                return $"data:image/png;base64,{base64}";
+            }
         }
     }
 }
