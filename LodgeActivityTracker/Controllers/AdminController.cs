aaad82de7e88faa672427ceab56ab1f5655d0a9a
LERATO LESHABANE <leshabanellh91@gmail.com>
2026-01-29 11:28:31 +0200

    updates files

---
+++ LodgeActivityTracker/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LodgeActivityTracker.Data;
+using LodgeActivityTracker.Models;
+
+using System;
+using System.Linq;
+
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AdminController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Dashboard()
+    {
+        var model = new AdminDashboardViewModel
+        {
+            TotalActivities = _context.Activities.Count(),
+            ActivitiesToday = _context.Activities
+                .Count(a => a.Date.Date == DateTime.Today),
+            RecentActivities = _context.Activities
+                .OrderByDescending(a => a.Date)
+                .Take(5)
+                .ToList()
+        };
+
+        return View(model);
+    }
+
+}
+    
+
---
+++ LodgeActivityTracker/Models/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using LodgeActivityTracker.Models;
+
+public class AdminDashboardViewModel
+{
+    public int TotalActivities { get; set; }
+    public int ActivitiesToday { get; set; }
+    public List<Activity> RecentActivities { get; set; }
+}
